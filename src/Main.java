0f7cfb511811de8650ef2b2c8909f19680d1287c
dilshara26 <dilsharahasanka@gmail.com>
2024-06-07 21:22:36 +0530

    week 3
    whileloop

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,59 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("If you are a hospital administrator please press 1, If you are a patient please press 2, Press 3 to exit");
-        int userType = scanner.nextInt();
+        boolean runMainMenu = true;
+        while (runMainMenu) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("If you are a hospital administrator please press 1, If you are a patient please press 2, Press 3 to exit");
+            int userType = scanner.nextInt();
 
-        if(userType == 1){
-            System.out.println("Press 1 to add a doctor, press 2 to add a doctor availability, and press 3 to exit");
-            int userObjective = scanner.nextInt();
-            if(userObjective == 1){
-                System.out.println("Add a doctor");
-            } else if (userObjective==2) {
-                System.out.println("Add doctor availability");
-            } else if (userObjective==3) {
-                System.out.println("Exit");
+            if(userType == 1){
+                boolean runAdminMenu = true;
+                while(runAdminMenu){
+                    System.out.println("Press 1 to add a doctor, press 2 to add a doctor availability, and press 3 to exit");
+                    int userObjective = scanner.nextInt();
+                    if(userObjective == 1){
+                        System.out.println("Add a doctor");
+                    } else if (userObjective==2) {
+                        System.out.println("Add doctor availability");
+                    } else if (userObjective==3) {
+                        runAdminMenu= false;
+                    }
+                    else {
+                        System.out.println("Invalid");
+                    }
+
+                }
+
+
+            }
+            else if(userType == 2){
+
+                System.out.println("Press 1 to view doctors, press 2 to book an appointment, press 3 to view a selected doctor’s bookings, and press 4 to exit");
+                int userObjective = scanner.nextInt();
+                if(userObjective == 1){
+                    System.out.println("view doctors list");
+                } else if (userObjective==2) {
+                    System.out.println("Book an appointment");
+                } else if (userObjective==3) {
+                    System.out.println("View a selected Doctor");
+                } else if (userObjective==4) {
+                    System.out.println("Exit");
+
+                } else {
+                    System.out.println("Invalid");
+                }
+
+            } else if (userType==3) {
+                runMainMenu = false;
+
             }
             else {
-                System.out.println("Invalid");
+                System.out.println("Invalid Input");
             }
 
-        }
-        else if(userType == 2){
-            System.out.println("Press 1 to view doctors, press 2 to book an appointment, press 3 to view a selected doctor’s bookings, and press 4 to exit");
-            int userObjective = scanner.nextInt();
-            if(userObjective == 1){
-                System.out.println("view doctors list");
-            } else if (userObjective==2) {
-                System.out.println("Book an appointment");
-            } else if (userObjective==3) {
-                System.out.println("View a selected Doctor");
-            } else if (userObjective==4) {
-                System.out.println("Exit");
-
-            } else {
-                System.out.println("Invalid");
-            }
-
-        } else if (userType==3) {
-            System.out.println("Exit");
 
         }
-        else {
-            System.out.println("Invalid Input");
-        }
-
     }
+
 }
