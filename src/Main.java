18a9a7da60885ee38688c7d1631f70f209c3f08f
dilshara26 <dilsharahasanka@gmail.com>
2024-06-14 20:15:09 +0530

    week 7
    changes added

--- src/Main.java
+++ src/Main.java
@@ -9,76 +9,94 @@ public class Main {
 
     public static void adminMenu(){
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Press 1 to add a doctor, press 2 to add a doctor availability, and press 3 to exit");
-        int userObjective = scanner.nextInt();
-        if(userObjective == 1){
+        boolean runAdmin = true;
+        while (runAdmin){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Press 1 to add a doctor, press 2 to add a doctor availability, and press 3 to exit");
+            int userObjective = scanner.nextInt();
+            if(userObjective == 1){
 //            get the relevant data
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter your Name: ");
-            String name = sc.nextLine();
-            System.out.println("Enter your birthday: ");
-            String birthday = sc.nextLine();
-            System.out.println("Enter your Name: ");
-            String specialization = sc.nextLine();
-            System.out.println("Enter your Name: ");
-            String contact = sc.nextLine();
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter your Name: ");
+                String name = sc.nextLine();
+                System.out.println("Enter your birthday: ");
+                String birthday = sc.nextLine();
+                System.out.println("Enter your Specialization: ");
+                String specialization = sc.nextLine();
+                System.out.println("Enter your Contact: ");
+                String contact = sc.nextLine();
 
-            Random random = new Random();
-            Doctor tempDoctor = new Doctor(random.nextInt(), name, birthday,specialization,contact);
+                Random random = new Random();
+                Doctor tempDoctor = new Doctor(random.nextInt(), name, birthday,specialization,contact);
 
-            allDoctors.add(tempDoctor);
+                allDoctors.add(tempDoctor);
 
 
-        } else if (userObjective==2) {
-            System.out.println("Add doctor availability");
-        } else if (userObjective==3) {
-            System.out.println("Exit");
+            } else if (userObjective==2) {
+                System.out.println("Add doctor availability");
+            } else if (userObjective==3) {
+                runAdmin = false;
+                System.out.println("Exit");
+            }
+            else {
+                System.out.println("Invalid");
+            }
+
         }
-        else {
-            System.out.println("Invalid");
-        }
+
 
     }
     public static void patientMenu(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Press 1 to view doctors, press 2 to book an appointment, press 3 to view a selected doctor’s bookings, and press 4 to exit");
-        int userObjective = scanner.nextInt();
-        if(userObjective == 1){
-            for (Doctor doc: allDoctors) {
-                System.out.println(doc.name + " has a specialization of " + doc.specialization);
+        boolean  runPatient = true;
+
+        while (runPatient){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Press 1 to view doctors, press 2 to book an appointment, press 3 to view a selected doctor’s bookings, and press 4 to exit");
+            int userObjective = scanner.nextInt();
+            if(userObjective == 1){
+                for (Doctor doc: allDoctors) {
+                    System.out.println(doc.name + " has a specialization of " + doc.specialization);
+                }
+
+            } else if (userObjective==2) {
+                System.out.println("Book an appointment");
+            } else if (userObjective==3) {
+                System.out.println("View a selected Doctor");
+            } else if (userObjective==4) {
+                runPatient= false;
+                System.out.println("Exit");
+
+            } else {
+                System.out.println("Invalid");
             }
 
-        } else if (userObjective==2) {
-            System.out.println("Book an appointment");
-        } else if (userObjective==3) {
-            System.out.println("View a selected Doctor");
-        } else if (userObjective==4) {
-            System.out.println("Exit");
-
-        } else {
-            System.out.println("Invalid");
         }
+
     }
 
     public static void run(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("If you are a hospital administrator please press 1, If you are a patient please press 2, Press 3 to exit");
-        int userType = scanner.nextInt();
 
-        if(userType == 1){
-           adminMenu();
-        }
-        else if(userType == 2){
-            patientMenu();
+        boolean runMain  = true;
 
-        } else if (userType==3) {
-            System.out.println("Exit");
-        }
-        else {
-            System.out.println("Invalid Input");
-        }
+        while (runMain){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("If you are a hospital administrator please press 1, If you are a patient please press 2, Press 3 to exit");
+            int userType = scanner.nextInt();
 
+            if(userType == 1){
+                adminMenu();
+            }
+            else if(userType == 2){
+                patientMenu();
+
+            } else if (userType==3) {
+                runMain = false;
+                System.out.println("Exit");
+            }
+            else {
+                System.out.println("Invalid Input");
+            }
+        }
     }
 
 
